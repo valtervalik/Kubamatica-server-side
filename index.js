6d1c29c8c9cb010a0afa8543296c64118f9c5102
Valter Valik <valtervaralik@gmail.com>
2023-04-07 22:07:34 -0400

    making home endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.get('/', (req, res) => {
+	res.send('Home');
+});
+
 app.listen(port, (req, res) => {
 	console.log(`Server is running on port ${port}`);
 });
