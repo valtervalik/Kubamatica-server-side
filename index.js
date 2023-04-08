ff763b3f1228a0cec6da397697210e2a7a2a034a
Valter Valik <valtervaralik@gmail.com>
2023-04-07 22:31:23 -0400

    connecting with mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+//connecting to the mongo database
+const mongoose = require('mongoose');
+
+main().catch((err) => console.log(err));
+
+async function main() {
+	await mongoose.connect('mongodb://127.0.0.1:27017/Kubamatica');
+	console.log('MongoDB connected');
+	// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 app.get('/', (req, res) => {
 	res.send('Home');
