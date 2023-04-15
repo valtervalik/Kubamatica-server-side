a0cf5c3ae29a5088c3c1e7e134f91a97266c1a82
Valter Valik <valtervaralik@gmail.com>
2023-04-15 01:01:20 -0400

    fixing res.send error

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ module.exports.registerUser = async (req, res, next) => {
 		const { email, username, role, fullname, phone, password } = req.body;
 		const user = new User({ role, fullname, phone, email, username });
 		const registeredUser = await User.register(user, password);
-		res.send('Usuario creado exitosamente');
+		res.json({ message: 'Usuario creado exitosamente' });
 	} catch (err) {
 		return next(err);
 	}
